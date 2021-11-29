c7dbcf3a689458269ea236d2092dc054b39b920a
Anay Wadhera <awadhera@berkeley.edu>
2021-11-29 10:02:57 -0800

    gs101-sepolicy: include google battery HAL sepolicy
    Change-Id: I3d26a175adf06780e7b44e1fddf927c017e81c0b
    Reviewed-on: https://review.statixos.com/c/android_device_google_gs101-sepolicy/+/6346
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_device_google_gs101-sepolicy/+/7437

--- gs101-sepolicy.mk
+++ gs101-sepolicy.mk
@@ -20,6 +20,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/priv
 #
 # Pixel-wide
 #
+#   Battery
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+
 #   Dauntless (uses Citadel policy currently)
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/citadel
 
